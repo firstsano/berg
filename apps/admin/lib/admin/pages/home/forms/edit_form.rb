e4b343cc25f3ea1e6db81ccbe1385860e24f9f51
Dylan Wolff <dylanwolff@gmail.com>
2016-05-30 09:25:25 +0900

    Add upload_field to featured items for home page edit form

--- apps/admin/lib/admin/pages/home/forms/edit_form.rb
+++ apps/admin/lib/admin/pages/home/forms/edit_form.rb
@@ -16,7 +16,10 @@ module Admin
               label: "Featured Items",
               action_label: "Add a featured item",
               placeholder: "No featured items added yet.",
-              validation: {} do
+              validation: {
+                min_size: 4,
+                max_size: 4
+              } do
                 text_field :title,
                   label: "Title"
 
@@ -25,10 +28,12 @@ module Admin
 
                 text_field :url,
                   label: "URL"
+              end
 
-                text_field :cover_image,
-                  label: "Cover Image"
-            end
+              upload_field :cover_image,
+                label: "Cover Image",
+                hint: "A cover image for this item",
+                presign_url: "#{Berg::Container["config"].canonical_domain}/admin/uploads/presign"
           end
         end
       end

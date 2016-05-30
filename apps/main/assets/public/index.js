764d8d797f50402f1da602ee372bc5351d3a7c18
Andy McCray <andymccray@regulus.local>
2016-05-30 10:11:27 +1000

    Remove public CSS, add public demo.

--- apps/main/assets/public/index.js
+++
@@ -1,9 +0,0 @@
-// import viewloader from 'viewloader'
-// import views from './views'
-
-/**
- * Kick things off on domReady
- */
-// document.addEventListener('turbolinks:load', function onTurboLinksLoad () {
-//   viewloader.execute(views)
-// })

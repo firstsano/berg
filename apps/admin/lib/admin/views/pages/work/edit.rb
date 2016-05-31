1a5d1fa1829dbe6a24e119e7a52004fec3835b4d
Dylan Wolff <dylanwolff@gmail.com>
2016-05-31 14:57:16 +0900

    Add edit view and template for work page

---
+++ apps/admin/lib/admin/views/pages/work/edit.rb
@@ -0,0 +1,43 @@
+require "admin/import"
+require "admin/pages/work/forms/edit_form"
+require "admin/view"
+
+module Admin
+  module Views
+    module Pages
+      module Work
+        class Edit < Admin::View
+          include Admin::Import(
+            "admin.persistence.repositories.work_page_featured_items",
+            "admin.pages.work.forms.edit_form"
+          )
+
+          configure do |config|
+            config.template = "pages/work/edit"
+          end
+
+          def locals(options = {})
+            featured_items = {
+              work_page_featured_items: work_page_featured_items.listing_by_position.map(&:to_h)
+            }
+
+            super.merge(
+              page_form: form_data(featured_items, options[:validation]),
+              csrf_token: options[:scope].csrf_token
+            )
+          end
+
+          private
+
+          def form_data(featured_items, validation)
+            if validation
+              edit_form.build(validation, validation.messages)
+            else
+              edit_form.build(featured_items)
+            end
+          end
+        end
+      end
+    end
+  end
+end

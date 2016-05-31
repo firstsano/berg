7d9f7720c8ee431fb5150efbf4723aa031995208
Dylan Wolff <dylanwolff@gmail.com>
2016-05-31 14:53:36 +0900

    Add work page edit form and validation

---
+++ apps/admin/lib/admin/pages/work/forms/edit_form.rb
@@ -0,0 +1,39 @@
+require "berg/form"
+
+module Admin
+  module Pages
+    module Work
+      module Forms
+        class EditForm < Berg::Form
+          include Admin::Import(
+            "admin.persistence.repositories.work_page_featured_items"
+          )
+
+          prefix :page
+
+          define do
+            many :work_page_featured_items,
+              label: "Featured Items",
+              action_label: "Add a featured item",
+              placeholder: "No featured items added yet.",
+              validation: {} do
+                text_field :title,
+                  label: "Title"
+
+                text_field :description,
+                  label: "Description"
+
+                text_field :url,
+                  label: "URL"
+
+                upload_field :cover_image,
+                  label: "Cover Image",
+                  hint: "A cover image for this item",
+                  presign_url: "#{Berg::Container["config"].canonical_domain}/admin/uploads/presign"
+                end
+          end
+        end
+      end
+    end
+  end
+end

b22ee6ef76fb3eb29acfad585d1a4ec35fbcb072
Dylan Wolff <dylanwolff@gmail.com>
2016-05-31 14:42:42 +0900

    Add work page featured items table, repositories and relation

---
+++ apps/main/lib/main/persistence/repositories/work_page_featured_items.rb
@@ -0,0 +1,16 @@
+require "berg/repository"
+
+module Main
+  module Persistence
+    module Repositories
+      class WorkPageFeaturedItems < Berg::Repository[:work_page_featured_items]
+        relations :work_page_featured_items
+
+        def listing_by_position
+          work_page_featured_items
+            .order(:position)
+        end
+      end
+    end
+  end
+end

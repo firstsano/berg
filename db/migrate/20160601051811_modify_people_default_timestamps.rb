2cedbc6dd192279caf3f7786fd13b0e0b31d01fb
Dylan Wolff <dylanwolff@gmail.com>
2016-06-01 14:23:38 +0900

    Modify default timestamp values for Users, Posts, Projects and People

---
+++ db/migrate/20160601051811_modify_people_default_timestamps.rb
@@ -0,0 +1,8 @@
+ROM::SQL.migration do
+  change do
+    alter_table(:people) do
+      set_column_default :created_at, Sequel.lit("(now() at time zone 'utc')")
+      set_column_default :updated_at, Sequel.lit("(now() at time zone 'utc')")
+    end
+  end
+end

5cbe10edbecedd440197d4a4f81a8e076f85fd76
Max Wheeler <max@makenosound.com>
2016-06-01 17:40:42 +1000

    Remove existing assets.

--- apps/main/assets/inline-header/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Inlined and injected in to the <head> of the page
- */
-
-var metaquery = require("metaquery");
-var checkFontsPreloaded = require("./check-fonts-preloaded");
-checkFontsPreloaded();

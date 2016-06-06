a1d399d329db380388e37c753c44517da32224d1
Dylan Wolff <dylan@dylanwolff.com>
2016-06-06 10:32:31 +0900

    Modify public Project entity

--- apps/main/lib/main/entities/project.rb
+++ apps/main/lib/main/entities/project.rb
@@ -8,11 +8,11 @@ module Main
       attribute :client, Types::Strict::String
       attribute :url, Types::Strict::String
       attribute :intro, Types::Strict::String
-      attribute :body, Types::Strict::String.optional
+      attribute :body, Types::Strict::String
       attribute :tags, Types::Strict::String
       attribute :slug, Types::Strict::String
       attribute :status, Types::ProjectStatus
-      attribute :published_at, Types::Strict::Time.optional
+      attribute :published_at, Types::Strict::Time
       attribute :case_study, Types::Strict::Bool
       attribute :cover_image, Types::Hash
     end

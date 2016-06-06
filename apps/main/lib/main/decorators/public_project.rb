92ba4fc7ae6a0a9d6dbed48f91a6aa4983351e29
Dylan Wolff <dylan@dylanwolff.com>
2016-06-06 09:24:58 +0900

    Make `attache_url_for` a private method for Persons and Projects

--- apps/main/lib/main/decorators/public_project.rb
+++ apps/main/lib/main/decorators/public_project.rb
@@ -7,6 +7,8 @@ module Main
         attache_url_for(cover_image["path"], size.to_s) if cover_image
       end
 
+      private
+
       def attache_url_for(file_path, geometry)
         prefix, basename = File.split(file_path)
         [Berg::Container["config"].attache_downloads_base_url, "view", prefix, CGI.escape(geometry), CGI.escape(basename)].join('/')

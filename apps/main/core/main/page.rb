4446d3fac45c5e2f92e2d9fc40e37449631b2203
Daniel Nitsikopoulos <daniel.nitsikopoulos@icelab.com.au>
2016-06-07 11:11:46 +1000

    Use propper class for main/footer nav

--- apps/main/core/main/page.rb
+++ apps/main/core/main/page.rb
@@ -3,7 +3,7 @@ require "berg/page"
 module Main
   class Page < Berg::Page
     def nav_link_for(label, path, options={})
-      active_class = "nav-main__anchor--active" if current_path.split("/").include?(path.gsub("/",""))
+      active_class = "#{options[:class]}--active" if current_path.split("/").include?(path.gsub("/",""))
       "<a class='#{options[:class]} #{active_class}' href='#{path}'>#{label}</a>"
     end
   end

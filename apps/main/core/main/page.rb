c0cbb5d31ae13b44bd16bebead79873e8b57cf25
Daniel Nitsikopoulos <daniel.nitsikopoulos@icelab.com.au>
2016-06-07 11:09:38 +1000

    Update nav_link_for method and re-use for footer nav

--- apps/main/core/main/page.rb
+++ apps/main/core/main/page.rb
@@ -2,9 +2,9 @@ require "berg/page"
 
 module Main
   class Page < Berg::Page
-    def nav_link_for(label, path)
+    def nav_link_for(label, path, options={})
       active_class = "nav-main__anchor--active" if current_path.split("/").include?(path.gsub("/",""))
-      "<a class='nav-main__anchor #{active_class}' href='#{path}'>#{label}</a>"
+      "<a class='#{options[:class]} #{active_class}' href='#{path}'>#{label}</a>"
     end
   end
 end

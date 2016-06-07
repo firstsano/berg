b09c616c723c0ed7f14e174b708e46650462fd15
Daniel Nitsikopoulos <daniel.nitsikopoulos@icelab.com.au>
2016-06-07 10:55:19 +1000

    Add active states to main nav menu items

--- apps/main/core/main/page.rb
+++ apps/main/core/main/page.rb
@@ -2,5 +2,9 @@ require "berg/page"
 
 module Main
   class Page < Berg::Page
+    def nav_link_for(label, path)
+      active_class = "nav-main__anchor--active" if current_path.split("/").include?(path.gsub("/",""))
+      "<a class='nav-main__anchor #{active_class}' href='#{path}'>#{label}</a>"
+    end
   end
 end

c2000ab7070e717ebbe1bb1f1b99395268ad9f0c
Daniel Nitsikopoulos <daniel.nitsikopoulos@icelab.com.au>
2016-06-09 15:47:21 +1000

    Prevent captured content from appearing on the page where it is being captured

--- lib/content_store.rb
+++ lib/content_store.rb
@@ -3,12 +3,13 @@ class ContentStore
     @content = {}
   end
 
-  def capture(key, value=nil, &block)
+  def capture(key, value = nil, &block)
     if block
       @content[key] = yield
     else
       @content[key] = value
     end
+    nil
   end
 
   def [](key, value = nil, &block)

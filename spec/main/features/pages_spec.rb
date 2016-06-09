3baf6369f3e65215e679eafa099d4a61fa37502a
Dylan Wolff <dylan@dylanwolff.com>
2016-06-09 11:37:40 +0900

    Consistently check for status code, even on success

--- spec/main/features/pages_spec.rb
+++ spec/main/features/pages_spec.rb
@@ -3,26 +3,31 @@ require "app_helper"
 RSpec.feature "Main / Pages" do
   scenario "Visiting the Home page" do
     visit "/"
+    expect(page.status_code).to eql 200
     expect(page).to have_title "Home – Icelab"
   end
 
   scenario "Visiting the Work page" do
     visit "/work"
+    expect(page.status_code).to eql 200
     expect(page).to have_title "Work – Icelab"
   end
 
   scenario "Visiting the About page" do
     visit "/about"
+    expect(page.status_code).to eql 200
     expect(page).to have_title "About – Icelab"
   end
 
   scenario "Visiting the Notes page" do
     visit "/notes"
+    expect(page.status_code).to eql 200
     expect(page).to have_title "Notes – Icelab"
   end
 
   scenario "Visiting the Contact page" do
     visit "/contact"
+    expect(page.status_code).to eql 200
     expect(page).to have_title "Contact – Icelab"
   end
 

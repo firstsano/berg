088cf5d166c2bda5f3058361adadf5ac36f8fa8a
Dylan Wolff <dylan@dylanwolff.com>
2016-06-09 11:36:17 +0900

    Add Pages spec for public app

---
+++ spec/main/features/pages_spec.rb
@@ -0,0 +1,52 @@
+require "app_helper"
+
+RSpec.feature "Main / Pages" do
+  scenario "Visiting the Home page" do
+    visit "/"
+    expect(page).to have_title "Home – Icelab"
+  end
+
+  scenario "Visiting the Work page" do
+    visit "/work"
+    expect(page).to have_title "Work – Icelab"
+  end
+
+  scenario "Visiting the About page" do
+    visit "/about"
+    expect(page).to have_title "About – Icelab"
+  end
+
+  scenario "Visiting the Notes page" do
+    visit "/notes"
+    expect(page).to have_title "Notes – Icelab"
+  end
+
+  scenario "Visiting the Contact page" do
+    visit "/contact"
+    expect(page).to have_title "Contact – Icelab"
+  end
+
+  scenario "Visiting a non-existent top-level page" do
+    visit "/foo"
+    expect(page.status_code).to eql 404
+    expect(page).to have_content "Oops! We couldn’t find this page"
+  end
+
+  scenario "Visiting a non-existent Project's page" do
+    visit "/work/foo"
+    expect(page.status_code).to eql 404
+    expect(page).to have_content "Oops! We couldn’t find this page"
+  end
+
+  scenario "Visiting a non-existent Post's page" do
+    visit "/notes/foo"
+    expect(page.status_code).to eql 404
+    expect(page).to have_content "Oops! We couldn’t find this page"
+  end
+
+  scenario "Visiting a non-existent Category's page" do
+    visit "/notes/category/foo"
+    expect(page.status_code).to eql 404
+    expect(page).to have_content "Oops! We couldn’t find this page"
+  end
+end

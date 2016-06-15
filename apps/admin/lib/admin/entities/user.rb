41472f0082322557f4bef053ded7fc014ab84a87
Dylan Wolff <dylan@dylanwolff.com>
2016-06-15 22:23:06 +0900

    Use strict types in User entity

--- apps/admin/lib/admin/entities/user.rb
+++ apps/admin/lib/admin/entities/user.rb
@@ -7,9 +7,9 @@ module Admin
       attribute :name, Types::Strict::String
       attribute :email, Types::Strict::String
       attribute :encrypted_password, Types::Nil | Types::Strict::String
-      attribute :access_token, Types::String
-      attribute :access_token_expiration, Types::Time
-      attribute :active, Types::Bool
+      attribute :access_token, Types::Strict::String
+      attribute :access_token_expiration, Types::Strict::Time
+      attribute :active, Types::Strict::Bool
 
       alias_method :active?, :active
     end

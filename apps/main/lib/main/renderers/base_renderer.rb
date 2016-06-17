2093d9f88451a578003d083fd78d85cb05f6937a
Dylan Wolff <dylan@dylanwolff.com>
2016-06-17 15:32:46 +0900

    An initial stab at rendering custom markdown for images

--- apps/main/lib/main/renderers/base_renderer.rb
+++ apps/main/lib/main/renderers/base_renderer.rb
@@ -5,4 +5,24 @@ require "rouge/plugins/redcarpet"
 class BaseRenderer < Redcarpet::Render::HTML
   include Redcarpet::Render::SmartyPants
   include Rouge::Plugins::Redcarpet
+
+  def image(link, title = nil, alt_text = nil)
+    parse_image_link(link)
+
+    "<figure>
+      <img src=\"#{@url}\" alt=\"#{@alt_text}\" class=\"#{@class}\">
+      <div>
+        <figurecaption>#{@caption}</figurecaption>
+      </div>
+    </figure>"
+  end
+
+  def parse_image_link(link)
+    matches = link.match(/^(.*?)\|(.*?)\|(.*?)\|(.*)/)
+
+    @url = matches[1]
+    @alt_text = matches[2]
+    @class = matches[3]
+    @caption = matches[4]
+  end
 end

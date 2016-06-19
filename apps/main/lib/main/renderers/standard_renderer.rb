c7ccdb73d8f423c6dff77535054048c1810a6937
Dylan Wolff <dylan@dylanwolff.com>
2016-06-20 08:23:42 +0900

    Modify syntax for assigning class

--- apps/main/lib/main/renderers/standard_renderer.rb
+++ apps/main/lib/main/renderers/standard_renderer.rb
@@ -7,16 +7,21 @@ class StandardRenderer < Redcarpet::Render::HTML
   include Rouge::Plugins::Redcarpet
 
   # Input syntax:
-  # ![](http://example.com/image.jpg|this is some alt text|this-is-the-class|this is the caption)
+  # ![](http://example.com/image.jpg|this is some alt text|left|this is the caption)
 
   def image(link, title = nil, alt_text = nil)
     parse_image_link(link)
 
-    "<figure>
-      <img src=\"#{@url}\" alt=\"#{@alt_text}\" class=\"#{@class}\">
-      <div>
-        <figurecaption>#{@caption}</figurecaption>
-      </div>
+    @translated_class =
+      if @class == "left"
+        'class=figure-align-left'
+      elsif @class == "right"
+        'class=figure-align-left'
+      end
+
+    "<figure #{@translated_class}>
+      <img src=\"#{@url}\" alt=\"#{@alt_text}\">
+      <figurecaption>#{@caption}</figurecaption>
     </figure>"
   end
 

84be1db00f2d58886a405063f3392e9b67bae0dd
Dylan Wolff <dylan@dylanwolff.com>
2016-06-21 08:17:25 +0900

    Update relation and entities to make url an optional value

--- apps/admin/lib/admin/entities/project.rb
+++ apps/admin/lib/admin/entities/project.rb
@@ -7,7 +7,7 @@ module Admin
       attribute :id, Types::Strict::Int
       attribute :title, Types::Strict::String
       attribute :client, Types::Strict::String
-      attribute :url, Types::Strict::String
+      attribute :url, Types::Strict::String.optional
       attribute :intro, Types::Strict::String
       attribute :body, Types::Strict::String.optional
       attribute :slug, Types::Strict::String

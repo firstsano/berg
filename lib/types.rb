090c8129524e257e28e90bdb9a43f411ed5194a9
Dylan Wolff <dylan@dylanwolff.com>
2016-06-21 13:32:42 +0900

    Update PostStatus type

--- lib/types.rb
+++ lib/types.rb
@@ -4,7 +4,7 @@ module Types
   include Dry::Types.module
 
   PostHighlightColor = Types::Strict::String.enum("red", "green", "blue", "grey")
-  PostStatus = Types::Strict::String.default("draft").enum("draft", "published", "deleted")
+  PostStatus = Types::Strict::String.default("draft").enum("draft", "published", "hidden")
   ProjectStatus = Types::Strict::String.default("draft").enum("draft", "published", "hidden")
 end
 

1e1a95d3e527d175644ce864456a863e3f33a659
Tim Riley <tim@openmonkey.com>
2016-08-01 09:05:24 +1000

    Tidy syntax

--- spec/main/shared/app/main_posts.rb
+++ spec/main/shared/app/main_posts.rb
@@ -3,17 +3,17 @@ require "babosa"
 RSpec.shared_context 'main posts' do
 
   def create_user(name: "Jane")
-    Berg::Container["persistence.commands.create_user"].({
+    Berg::Container["persistence.commands.create_user"].(
       name: name,
       email: "#{name}#{Random.new.rand(100)}@example.org",
       active: true,
       access_token: "123456",
-      access_token_expiration: Time.now
-    })
+      access_token_expiration: Time.now,
+    )
   end
 
   def create_post(title, teaser, slug, person, status="published")
-    Berg::Container["persistence.commands.create_post"].({
+    Berg::Container["persistence.commands.create_post"].(
       title: title,
       body: "test",
       teaser: teaser,
@@ -22,6 +22,6 @@ RSpec.shared_context 'main posts' do
       status: status,
       published_at: Time.now,
       color: "blue",
-    })
+    )
   end
 end

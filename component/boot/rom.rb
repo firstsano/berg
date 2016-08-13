1c8833292e06a8e2cc23f5e501b2cb5b26242c61
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-08-13 19:25:14 +0200

    Refactor rom boot to use lifecycle triggers

--- component/boot/rom.rb
+++ component/boot/rom.rb
@@ -1,21 +1,26 @@
-Berg::Container.namespace "persistence" do |container|
-  container.boot!(:config)
+Berg::Container.namespace "persistence" do |persistence|
+  persistence.finalize(:rom) do
+    start do
+      uses :config
 
-  require "sequel"
+      require "sequel"
+      require "rom"
 
-  Sequel.database_timezone = :utc
-  Sequel.application_timezone = :local
+      Sequel.database_timezone = :utc
+      Sequel.application_timezone = :local
 
-  require "rom"
+      rom_config = ROM::Configuration.new(
+        :sql, config.database_url, extensions: [:error_sql, :pg_array, :pg_json]
+      )
 
-  config = ROM::Configuration.new(
-    :sql, container["config"].database_url, extensions: [:error_sql, :pg_array, :pg_json]
-  )
+      persistence.register("config", rom_config)
+    end
 
-  container.register("config", config)
+    runtime do
+      config = container["persistence.config"]
+      config.auto_registration(container.root.join("lib/persistence"))
 
-  container.finalize(:rom) do
-    config.auto_registration(container.root.join("lib/persistence"))
-    container.register("rom", ROM.container(config))
+      persistence.register("rom", ROM.container(config))
+    end
   end
 end

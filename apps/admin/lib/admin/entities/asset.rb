d0573023015c4b83db0f8aaddaf4a935ed34cf30
Piotr Solnica <piotr.solnica+oss@gmail.com>
2018-02-05 17:28:49 +0100

    [wip] remove constructor_type from Asset entity

--- apps/admin/lib/admin/entities/asset.rb
+++ apps/admin/lib/admin/entities/asset.rb
@@ -3,10 +3,6 @@ require "berg/entity"
 module Admin
   module Entities
     class Asset < Berg::Entity
-      # Assets are often loaded from JSON columns in other tables, which means
-      # their keys are strings. Use a symbolized constructor here to make sure
-      # we can find the keys we need to initialize a valid object.
-      constructor_type :symbolized
     end
   end
 end

8504af265f416ca8dbaa2506752cd59b3ecb0355
Max Wheeler <max@makenosound.com>
2020-01-22 11:46:52 +1100

    Allow CORS in dev

--- assets/development-server.js
+++ assets/development-server.js
@@ -32,6 +32,11 @@ var devServerOptions = {
   noInfo: false,
   publicPath: "/assets/",
   historyApiFallback: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+  },
   stats: {
     assets:       false,
     assetsSort:   false,
